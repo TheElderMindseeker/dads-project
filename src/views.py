5b608fe72b04598a561c757259add8593484e2d2
aleksix <nope>
2019-10-01 15:49:14 +0300

    Added a register page and fixed the page directory.

--- src/views.py
+++ src/views.py
@@ -1,5 +1,7 @@
 """Contains application views"""
-from flask import Blueprint
+from flask import Blueprint, request, render_template
+
+from src.extensions import bcrypt
 
 views = Blueprint('views', __name__)  # pylint: disable=invalid-name
 
@@ -8,3 +10,13 @@ views = Blueprint('views', __name__)  # pylint: disable=invalid-name
 def hello_world():
     """Outputs hello world greeting to the client"""
     return '<h1>Hello, World!</h1>'
+
+
+@views.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.j2')
+    elif request.method == 'POST':
+        print("POST")
+        print(request.form)
+        return "NO"
