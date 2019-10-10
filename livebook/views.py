d7b420e645b59976c5f70e19aa5aa4b83093b7d7
aleksix <nope>
2019-10-10 17:33:36 +0300

    Added basic login functionality.
    The user can log in with valid credentials, and their session will be remembered and any later login attempts will return automatically.

--- livebook/views.py
+++ livebook/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, render_template
 from livebook.models import User
 
 from livebook.extensions import db, bcrypt
+from flask_login import login_user, current_user
 
 views = Blueprint('views', __name__)  # pylint: disable=invalid-name
 
@@ -23,9 +24,27 @@ def register():
     user_check = User.query.filter_by(email=data['email']).first()
     if user_check is None:
         # pylint: disable=no-member
-        # FIXME: generate_password_hash returns bytes which can bring possible hash check problem during login
-        new_user = User(email=data['email'], password=bcrypt.generate_password_hash(data['password']), admin=False)
+        new_user = User(email=data['email'],
+                        password=bcrypt.generate_password_hash(data['password']).decode(),
+                        admin=False)
         db.session.add(new_user)
         db.session.commit()
         return "registered"
     return "failed to register"
+
+
+@views.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user is not None and current_user.is_authenticated:
+        return "already logged in"
+
+    if request.method == 'GET':
+        return render_template('login.j2')
+
+    data = request.form
+    user = User.query.filter_by(email=data['email']).first()
+    remember = data.get("remember", False) == 'on'
+    if user is not None and bcrypt.check_password_hash(user.password, data['password']):
+        login_user(user, remember=remember)
+        return "logged in"
+    return "failed to log in"
