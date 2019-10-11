86a64049b3ab6530f61927ce374ffe9984998100
Daniil Alexandrovich <daniil.botnarenku@gmail.com>
2019-10-11 21:19:48 +0300

    Change license to GNU GPL v3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     url='https://github.com/TheElderMindseeker/dads-project',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
