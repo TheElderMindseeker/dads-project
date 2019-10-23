cf46f459280feede430d997c374b5d404050dfea
aleksix <nope>
2019-10-23 21:58:49 +0300

    Added a command for database creation.
    Modified the README file to include the section on setting up.
    Changed the DB config to be static for debugging.

--- livebook/__init__.py
+++ livebook/__init__.py
@@ -3,19 +3,24 @@ from flask import Flask
 from flask_migrate import Migrate
 
 from livebook.extensions import *
-from livebook.models import db
+from livebook.models import db, init_db
 from livebook.views import views
 
 
 def create_app(config_scheme='debug'):
     """Application factory function"""
+    # App creation
     app = Flask(__name__)
     app.config.from_pyfile("../config/{}.py".format(config_scheme))
     app.register_blueprint(views)
 
+    # Extensions initialization
     db.init_app(app)
-    Migrate(app, db)
+    migration.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+
+    # Additional processing
+    app.cli.add_command(init_db)
 
     return app
