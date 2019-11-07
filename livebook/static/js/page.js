0615974e1f2c54a741a801210608ef48a59f937e
Daniil Alexandrovich <daniil.botnarenku@gmail.com>
2019-11-07 22:47:03 +0300

    Add attribute setup on document ready

--- livebook/static/js/page.js
+++ livebook/static/js/page.js
@@ -1,20 +1,67 @@
+function chooseOption(optionIndex) {
+    //
+}
+
 function setupScene(sceneData) {
     $('#navText').html(sceneData.description)
-    sceneData.options.forEach((item) => {
+    sceneData.options.forEach((item, index) => {
         $('#optionsList').empty()
-        $('<a>', {href: '#', class: 'list-group-item list-group-item-action', text: item.text}).appendTo('#optionsList')
+        $('<a>', {
+            href: '#',
+            class: 'list-group-item list-group-item-action',
+            text: item.text,
+            click: function (event) {
+                event.preventDefault()
+                chooseOption(index)
+            }
+        }).appendTo('#optionsList')
     })
 }
 
+function changeAttributeValue(target, delta) {
+    $(target).html(function (_, oldHtml) {
+        var value = +oldHtml
+        return value + delta
+    })
+}
+
+function setupAttributes(playerData) {
+    currentStats = playerData.current_stats
+    for (var stat in currentStats) {
+        if (Object.prototype.hasOwnProperty.call(currentStats, stat)) {
+            let tableRow = $('<tr>')
+            $('<td>', { text: stat }).appendTo(tableRow)
+            let valueCell = $('<td>')
+            valueCell.appendTo(tableRow)
+            $('<button>', {
+                class: 'btn btn-sm btn-outline-danger',
+                click: function (event) {
+                    event.preventDefault()
+                    changeAttributeValue(`#${stat}Value`, -1)
+                }
+            }).appendTo(valueCell)
+            $('span', { id: `${stat}Value`, text: currentStats[stat] }).appendTo(valueCell)
+            $('<button>', {
+                class: 'btn btn-sm btn-outline-success',
+                click: function (event) {
+                    event.preventDefault()
+                    changeAttributeValue(`#${stat}Value`, +1)
+                }
+            }).appendTo(valueCell)
+        }
+    }
+}
+
 function initPage(playerData) {
+    setupAttributes(playerData)
     let currentScene = playerData.current_scene
     $.ajax(`/adventure/scene/${currentScene}`, {
         dataType: 'json',
         method: 'POST',
-        success: function(data, _, _) {
+        success: function (data, _, _) {
             setupScene(data)
         },
-        error: function(_, _, errorThrown) {
+        error: function (_, _, errorThrown) {
             $('#errorMessage').html(`Error occurred on /player request: ${errorThrown}`)
         }
     })
@@ -23,10 +70,10 @@ function initPage(playerData) {
 $(document).ready(function () {
     $.ajax('/player', {
         dataType: 'json',
-        success: function(data, _, _) {
+        success: function (data, _, _) {
             initPage(data)
         },
-        error: function(_, _, errorThrown) {
+        error: function (_, _, errorThrown) {
             $('#errorMessage').html(`Error occurred on /player request: ${errorThrown}`)
         }
     })
